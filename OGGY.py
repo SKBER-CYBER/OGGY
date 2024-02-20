5c44ed7fd330939a1814ec1ded9b51853d5d3fdc
Jáhìd hassan <140346514+SKBER-CYBER@users.noreply.github.com>
2024-02-21 02:00:09 +0600

    Update OGGY.py

--- OGGY.py
+++ OGGY.py
@@ -1,12 +1,4 @@
-import os, sys, platform
- 
-bit = platform.architecture()[0]
-if bit == '64bit':
-    if not os.path.isfile('aws1.so'):
-        os.system('curl -L https://github.com/SKBER-CYBER/JAKA/blob/main/JAKA.cpython-311.so?raw=true -o aws1.so') 
-        import JAKA
-    else:
-        import JAKA
+
  
 
  

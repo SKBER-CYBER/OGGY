ac550136b669dc286a5639064b179dbd0303bfe7
LêgêñÐ ᎧᎶᎶᎩ <140346514+SKBER-CYBER@users.noreply.github.com>
2024-04-10 10:21:44 +0600

    Update OGGY.py

--- OGGY.py
+++ OGGY.py
@@ -5,7 +5,7 @@ os.system('git pull')
 bit = platform.architecture()[0]
 if bit=='64bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools \033[1;37m')
-    os.system('xdg-open https://www.facebook.com/oggytool')
+    os.system('xdg-open https://github.com/SKBER-CYBER')
     import Fuck
 else:
     exit('\033[1;31m[×] Sorry Device Not Support')

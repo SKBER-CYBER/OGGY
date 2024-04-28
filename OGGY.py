37f138b2d754cd7384c045ae93759dbc969ff527
LêgêñÐ ᎧᎶᎶᎩ <140346514+SKBER-CYBER@users.noreply.github.com>
2024-04-28 16:47:44 +0600

    Update OGGY.py

--- OGGY.py
+++ OGGY.py
@@ -1,5 +1,5 @@
 import os,time
 os.system('clear')
 for up in range(999):
-print('[+] Place W8')
-time.sleep(0.1)
+  print("  \033[1;91m[\033[1;92m●\033[1;91m]\033[1;92m PLEASE WAIT YOUR NEXT UPDATE ")
+  time.sleep(0.1)
